2d58812067654e58282505b72456150c243c9578
Aezapata57 <aezapata57@ucatolica.edu.co>
2023-11-14 20:57:04 -0500

    Codigo de prtueba, no oficial

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '61d548e6f4c9ab29444a429adc21a8d6cefaf487',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '61d548e6f4c9ab29444a429adc21a8d6cefaf487',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
